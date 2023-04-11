563a1c8078e7f1463f1714c9281761de5258b49f
Ali503-7 <alimoali354@gmail.com>
2023-04-11 05:45:21 +0200

    V1 of my web site :)

---
+++ src/components/Bata Components/Social/social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
+import "./social.scss";
+
+function social() {
+  return (
+    <div className="social container">
+      <div className="social-links">
+        <a
+          href="https://www.linkedin.com/in/ali-mohamed-b351a6206/"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </a>
+        <a href="https://github.com/Ali503-7" target="_blank">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a
+          href="https://www.frontendmentor.io/profile/Ali503-7"
+          target="_blank"
+        >
+          <img src="/imgs/logs/frontend-mentor-seeklogo.com.svg" alt="" />
+        </a>
+      </div>
+      <span className="line"></span>
+    </div>
+  );
+}
+
+export default social;
