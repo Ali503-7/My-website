7050499eb3c952f9b15732b5805d1147df7210f1
Ali503-7 <Ali503-7@users.noreply.github.com>
2023-04-12 03:10:57 +0000

    deploy: bf2bb3319a83eeb4b5f602774edf8a414adb70de

--- assets/Swiper-a90f69c5.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-fb2f348d.js";function t({swipeFrom:e,toWare:r}){return i("div",{className:`swipe ${e}`,children:r})}export{t as default};
---
+++ assets/Swiper-e92fbe52.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-f3cba0a7.js";function t({swipeFrom:e,toWare:r}){return i("div",{className:`swipe ${e}`,children:r})}export{t as default};
